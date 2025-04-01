5868ea3a39539028f741991be440ded318374398
Juan Pablo Franco Herrera <juan_pablo.franco@uao.edu.co>
2025-04-01 16:20:55 -0500

    V2 (completado)
    Balanceador completamente funcional

--- Practica 1/Server1.js
+++ Practica 1/Server1.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
     res.send('Respuesta desde servidor 1 en puerto 3000');
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log('Servidor 1 corriendo en puerto 3000');
 });
--- Practica 1/Server2.js
+++ Practica 1/Server2.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
     res.send('Respuesta desde servidor 2 en puerto 3001');
 });
 
-app.listen(3001, () => {
-    console.log('Servidor 2 corriendo en puerto 3001');
+app.listen(3002, () => {
+    console.log('Servidor 2 corriendo en puerto 3002');
 });
